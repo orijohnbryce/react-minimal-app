e9f5781edb18f54a73cd42073eaa42ac9d0dcda5
ori_b <ori@ori>
2023-05-04 13:22:11 +0300

    prettier axios example

--- src/client/axios-example.js
+++ src/client/axios-example.js
@@ -1,19 +1,16 @@
 import axios from "axios";
 
 export async function axiosExample() {
-  const data = {
-    name: "Ori",
-    message: "Hello from Axios!",
+  const url = "https://httpbin.org/anything";
+  const data = { name: "Ori" };
+  const headers = {
+    "Content-Type": "application/json",
+    Authorization: "Bearer YOUR_ACCESS_TOKEN",
   };
 
   try {
-    console.log("About to call by axios")
-    const response = await axios.post("https://httpbin.org/anything", data, {
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: "Bearer YOUR_ACCESS_TOKEN",
-      },
-    });
+    console.log("About to call with Axios");
+    const response = await axios.post(url, data, { headers });
     console.assert(response.status == 200);
     console.log("Response:", response.data.data);
   } catch (error) {
